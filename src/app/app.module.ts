f1d1184341d5820d4a4ce223e772164fcdcb4ca8
angularsantanderii <angularsantanderii@gmail.com>
2022-02-23 08:43:29 +0000

    service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TablaClientesComponent } from './clientes/tabla-clientes/tabla-clientes.component';
+import { CrearClienteComponent } from './clientes/crear-cliente/crear-cliente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TablaClientesComponent,
+    CrearClienteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
